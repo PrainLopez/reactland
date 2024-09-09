fb8723150befd79d28219a97bc40094121233039
PrainLopez <qym200042@gmail.com>
2024-09-09 14:48:46 +0100

    I miss Vue SFC

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 export default function App() {
-
-  const spinMapping = ["logo", "logo react"]
+  const spinMapping = ["logo", "logo react"];
   const [spin, setSpin] = useState(1);
   const [count, setCount] = useState(15);
 
@@ -24,10 +23,14 @@ export default function App() {
         <button onClick={() => setCount((count) => count + 2)}>
           count is {count}
         </button>
-        <button 
-          onClick={()=>{setSpin(1-spin)}}
+        <button
+          onClick={() => {
+            setSpin(1 - spin);
+          }}
           style={{ backgroundColor: "#555" }}
-        >switch react light</button>
+        >
+          switch react light
+        </button>
         <p>
           Edit <code>src/App.jsx</code> and <code>src/main.jsx</code> and save
           to test HMR
