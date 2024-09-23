50b5a26d32ea6008cae3fc7e6b0023ceeda35160
PrainLopez <qym200042@gmail.com>
2024-09-23 16:54:36 +0100

    修理改进抽屉，校对数据

--- components/glues/selector.tsx
+++ components/glues/selector.tsx
@@ -19,6 +19,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { Button } from "@/components/ui/button";
 import { itemGrid } from "@/components/data/itemList";
 
@@ -28,38 +29,55 @@ export default function Selector() {
       <DrawerTrigger asChild>
         <Button className="">Select Item</Button>
       </DrawerTrigger>
-      <DrawerContent className="border-neutral-700">
-        <div className="mx-auto w-full max-h-[75vh]">
-          <DrawerHeader></DrawerHeader>
-          {itemGrid.map((cat) => (
-            <Card
-              key={cat.catName}
-              className="border-amber-900 bg-amber-950 mx-auto w-auto max-w-sm "
-            >
-              <CardHeader>
-                <CardTitle className="mx-1 text-xl text-amber-500">
-                  {cat.catName}
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                {cat.content.map((item) => (
-                  <Button
-                    key={item.name}
-                    className="bg-neutral-400/50 hover:bg-neutral-500 w-[4.5rem] h-[4.5rem] m-[0.35rem]"
-                  >
-                    <Image
-                      className="absolute"
-                      src={item.src}
-                      alt={item.name}
-                      width={55}
-                      height={55}
-                    />
-                  </Button>
-                ))}
-              </CardContent>
-            </Card>
-          ))}
-        </div>
+      <DrawerContent
+        className="
+      border-neutral-700 
+      w-full max-h-[75vh] mx-auto
+      "
+      >
+        <DrawerHeader></DrawerHeader>
+        <ScrollArea className="h-full overflow-auto">
+          <div className="mx-8 flex flex-wrap justify-evenly">
+            {itemGrid.map((cat) => (
+              <Card
+                key={cat.catName}
+                className="
+                border-amber-900 bg-amber-950 
+                mx-2 my-1 
+                w-auto min-w-96 max-w-md
+                "
+              >
+                <CardHeader>
+                  <CardTitle className="mx-1.5 text-xl text-amber-500">
+                    {cat.catName}
+                  </CardTitle>
+                </CardHeader>
+                <CardContent className="grid grid-cols-4">
+                  {cat.content.map((item) => (
+                    <Button
+                      key={item.name}
+                      className="
+                      flex flex-col justify-center 
+                      overflow-hidden hover:overflow-visible 
+                      bg-neutral-400/50 hover:bg-neutral-500 
+                      w-[5rem] h-[7rem] p-0 m-1
+                      "
+                    >
+                      <Image
+                        className="m-1"
+                        src={item.src}
+                        alt={item.name}
+                        width={55}
+                        height={55}
+                      />
+                      <span className="my-1">{item.name}</span>
+                    </Button>
+                  ))}
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </ScrollArea>
       </DrawerContent>
     </Drawer>
   );
